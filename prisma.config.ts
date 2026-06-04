c3c35ac01cf611672437e162b75f59f50a19e4a2
Gayasuddin <gyasuansari145@gmail.com>
2026-06-04 10:30:15 +0530

    fix: downgrade to Prisma v6 for Vercel serverless compatibility

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,4 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: "mysql://fyeGqCRoeAzHCse.root:Xex6GFjHCFYngM2r@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/renoassignment?sslaccept=strict",
-  },
-});
+export default {};
